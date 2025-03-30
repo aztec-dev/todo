eee1b9e1ddd9e9e55f1ad6c71c7a8296da1dd7a5
aztec-dev <aztech.dev1@gmail.com>
2025-03-30 13:26:49 +1000

    wip: added paper for testing

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import OutlinedCard from './components/TaskCard.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <App></App>
+      <OutlinedCard></OutlinedCard>
   </StrictMode>,
 )
